758e1b2af108cee0a9ed995a9795600d340a8be0
kinsman646 <teesam2004@gmail.com>
2023-10-25 16:40:02 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,37 @@
 import React from "react";
 
 function NavBar() {
-  return (
+ return (
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
-  );
+ );
 }
 
 function Home() {
-  return (
+ return (
     <div id="home">
       <h1>Home</h1>
     </div>
-  );
+ );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+ return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+ );
+}
 
 function App() {
-  return (
+ return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
-  );
+ );
 }
 
 export default App;
